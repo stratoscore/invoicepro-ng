ee75ed9cbc801d2acdb13e8f2849a04869800518
jeremiahbwala <charlesjeremiah204@gmail.com>
2026-07-07 15:18:40 +0100

    Initialize posthog

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { initPostHog } from './analytics/posthog.ts';
 
+initPostHog()
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
