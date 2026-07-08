84264170bb3c276a38c1a3ab5c289031af37c62d
jeremiahbwala <charlesjeremiah204@gmail.com>
2026-07-08 09:58:36 +0100

    Update posthog

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { initPostHog } from './analytics/posthog.ts';
+import { PostHogProvider } from '@posthog/react'
 
-initPostHog()
+const options = {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+  defaults: '2026-05-30',
+} as const
+
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <PostHogProvider apiKey={import.meta.env.VITE_POSTHOG_PROJECT_TOKEN} options={options}>
       <App />
+    </PostHogProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
